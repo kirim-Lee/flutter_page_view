612238baf662708ec19f5f51c0e4ac63ed15bb84
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2023-04-16 19:26:21 +0900

    Statefull Widget으로 변경 및 타이머 추가

--- README.md
+++ README.md
@@ -14,3 +14,28 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## 상태바에 관한 옵션
+
+시스템의 UI 그래픽 설정 변경하는 기능을 제공
+
+`import 'package:flutter/services.dart';`
+```dart
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+const HomeScreen({Key? key}):super(key:key);
+
+	// 상태바의 색상 dart=> 검정, light=>흰색
+	SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light); 
+	// 앱 풀스크린모드 지정 (시계, 배터리 잔량 등의 표기 설정)
+SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+// 앱 실행방향 결정
+SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight]);
+// 시스템 UI 변경시 콜백함수
+SystemChrome.setSystemUIChangeCallback((bool systemOverlaysAreVisible) async {});
+}
+```
+
+
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,30 +1,41 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomeScreen extends StatelessWidget {
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}):super(key:key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+
+
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      print('실행');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light); // 상태바의 색상 dart=> 검정, light=>흰색
-    // 앱 풀스크린모드 지정 (시계, 배터리 잔량 등의 표기 설정)
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
-    // 앱 실행방향 결정
-    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight]);
-    // 시스템 UI 변경시 콜백함수
-    SystemChrome.setSystemUIChangeCallback((bool systemOverlaysAreVisible) async {});
-
-    print('loader');
 
     return Scaffold(
       body: PageView(
         children: [1,2,3,4,5]
             .map(
               (number) => Image.asset(
-                  'assets/img/img$number.jpg',
-                  fit:BoxFit.cover
-              ),
-            ).toList(),
+              'assets/img/img$number.jpg',
+              fit:BoxFit.cover
+          ),
+        ).toList(),
       ),
     );
   }
